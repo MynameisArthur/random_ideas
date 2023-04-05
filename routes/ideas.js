395275b5ff80fb304b3be07b6c9a1dc0c0aca272
unknown <arthurd800@wp.pl>
2023-04-05 14:39:39 +0200

    updated server.js and routes/ideas.js files, modified .gitignore file, added data.config (ignored by git)

--- routes/ideas.js
+++ routes/ideas.js
@@ -17,4 +17,44 @@ router.get('/:id', (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// add an idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+  ideas.push(idea);
+  res.json({ success: true, data: idea });
+});
+// remove an idea
+router.delete('/:id', (req, res) => {
+  const ideaToRemove = ideas.find((idea) => idea.id === +req.params.id);
+  if (!ideaToRemove) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  const ideasLeft = ideas.filter((idea) => idea.id !== ideaToRemove.id);
+  res.json({
+    success: true,
+    message: `Removed idea with id ${req.params.id}`,
+    removed: ideaToRemove,
+    ideasLeft,
+  });
+});
+// update an idea
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+  res.json({ success: true, data: idea });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const port = 5000;
 
 const app = express();
+
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the Random Ideas' });
 });
